41069907a3cc5a799c9ec69deac0e195d8e63c93
brendastoute <89835895+brendastoute@users.noreply.github.com>
2021-09-27 14:04:53 +0200

    Add files via upload

---
+++ toets.py
@@ -0,0 +1,74 @@
+print ("welkom bij de auditie")
+# var 1 amount: 4
+#var 2 amount: 6
+#var 3 amount: 8
+geelen = 4
+mevrouwwit= 6
+groenwoud = 8
+var1= 0
+var2= 0 
+var3= 0
+
+zin = input("wilt u aanmelden voor een rol?\nja,nee\n")
+if zin == "ja":
+
+    leeftijd = int(input("hoe oud is u?\n"))
+    diploma = input("heeft u een diploma\nja,nee\n")
+    gender = input("bent u een man of een vrouw?\n")
+    verlegen = input("bent u verlegen?\nja,nee\n") 
+    boos = input("word u snel boos?\nja,nee\n")
+
+    if leeftijd >= 18:
+        var1 = var1 + 1
+
+    elif leeftijd <=18:
+        var2 = var2 + 0
+
+    elif leeftijd >= 20:
+        var3 = var3 + 3
+
+
+    if diploma == "ja":
+        var1 = var1 + 0
+
+    if diploma == "nee": 
+        var2 = var2 + 2
+        var3 = var3 + 0
+
+    if gender == "man":
+        var2 = var2 + 3
+        var1 = var1 + 0
+
+    if gender == "vrouw":
+        var3 = var3 + 2
+
+    if verlegen == "ja":
+        var1 = var1 + 2
+        var2 = var2 + 1
+
+    elif verlegen == "nee":
+        var3 = var3 + 0
+
+    if boos =="ja":
+        var3 == var3 + 3
+    elif boos == "nee":
+        var2 = var2 + 4
+        var1 = var1 + 1
+
+
+if var1 >= 0 or var1 <=5 :
+    print ("u hebt te rol van geelen")
+
+if var2 >= 4:
+    print("u hebt de rol van mevrouw de wit")
+
+if var3 >= 6:
+    print("u heeft de rol van groenwoud")
+
+
+elif zin == "nee":
+    print("uhm ok?")
+
+print(var2)
+
+
